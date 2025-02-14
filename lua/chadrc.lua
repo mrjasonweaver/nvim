f6e18c5dfc1a4522605279f39024b8b545ec95df
Jason Weaver <indyplanets@gmail.com>
2025-02-14 15:18:37 -0600

    configuration.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,11 +7,60 @@ local M = {}
 
 M.base46 = {
   theme = "chadracula-evondev",
+}
+M.ui = {
+  cmp = {
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    format_colors = {
+      tailwind = false,
+    },
+  },
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- ["@comment"] = { italic = true },
-  -- },
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
+    modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+    bufwidth = 21,
+  },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+
+  header = {
+    "                            ",
+    "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+    "   ▄▀███▄     ▄██ █████▀    ",
+    "   ██▄▀███▄   ███           ",
+    "   ███  ▀███▄ ███           ",
+    "   ███    ▀██ ███           ",
+    "   ███      ▀ ███           ",
+    "   ▀██ █████▄▀█▀▄██████▄    ",
+    "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+    "                            ",
+    "     Powered By  eovim    ",
+    "                            ",
+  },
+
+  buttons = {
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    -- more... check nvconfig.lua file for full list of buttons
+  },
 }
 
 return M
